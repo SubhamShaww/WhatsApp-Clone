b93dd71bbc2f62b8b0e204ff8cc8131fd3588620
Subham Shaw <subham@bankoncube.com>
2021-05-09 15:00:51 +0530

    code ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./Sidebar";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Login from "./Login";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [{ user }, dispatch] = useStateValue();
+
+    return (
+        // BEM naming convention
+        <div className="app">
+            {!user ? (
+                <Login />
+            ) : (
+                <div className="app__body">
+                    <Router>
+                        <Sidebar />
+
+                        <Route path="/rooms/:roomId">
+                            <Chat />
+                        </Route>
+                    </Router>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
